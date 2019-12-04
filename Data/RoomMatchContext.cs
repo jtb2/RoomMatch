57e074e0cc8198fa9ae171ef41089f970bf37eb2
Zach Davis <zfdavis21@gmail.com>
2019-12-03 20:17:59 -0600

    Setup code gen

---
+++ Data/RoomMatchContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace RoomMatch.Models
+{
+    public class RoomMatchContext : DbContext
+    {
+        public RoomMatchContext(DbContextOptions<RoomMatchContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Movie { get; set; }
+    }
+}
