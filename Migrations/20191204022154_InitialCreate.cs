f907651e9b140fd3862353112da2d451acaa8faa
Zach Davis <zfdavis21@gmail.com>
2019-12-03 20:24:39 -0600

    Do the migration thing

---
+++ Migrations/20191204022154_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RoomMatch.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    DOB = table.Column<DateTime>(nullable: false),
+                    A1 = table.Column<bool>(nullable: false),
+                    A2 = table.Column<bool>(nullable: false),
+                    A3 = table.Column<bool>(nullable: false),
+                    A4 = table.Column<bool>(nullable: false),
+                    A5 = table.Column<bool>(nullable: false),
+                    A6 = table.Column<bool>(nullable: false),
+                    A7 = table.Column<bool>(nullable: false),
+                    A8 = table.Column<bool>(nullable: false),
+                    A9 = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
