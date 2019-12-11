200bc5c4b79e268a200f652fb3b849aa56c373bb
Zach Davis <zfdavis21@gmail.com>
2019-12-11 12:17:57 -0600

    Transfer stuff

---
+++ RoomMatch/Data/RoomMatchContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RoomMatch.Models
+{
+    public class RoomMatchContext : DbContext
+    {
+        public RoomMatchContext(DbContextOptions<RoomMatchContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+}
---
+++ RoomMatch/Pages/Create.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RoomMatch.Models;
+
+namespace RoomMatch.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RoomMatchContext _context;
+
+        public CreateModel(RoomMatchContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Student.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
